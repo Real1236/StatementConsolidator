b8c41919c9d50ce7121ce7b976f55c21d5599161
Real1236 <arthurchan2001@live.com>
2024-06-14 12:41:39 -0400

    eliminates duplicate entries from previous statement tracker

--- consolidation_module/consolidator.py
+++ consolidation_module/consolidator.py
@@ -7,9 +7,13 @@ class Consolidator(Filter):
     def process(self, data: List[Statement]) -> List[Transaction]:
         # Consolidate and sort transactions from all statements
         transactions = []
+        transactions_set = set()
         for statement in data:
             for transaction in statement.statement:
+                if str(transaction) in transactions_set:
+                    continue
                 transactions.append(transaction)
+                transactions_set.add(str(transaction))
 
         transactions.sort(key=lambda x: x.transactionDate)
         return transactions
