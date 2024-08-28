1aa69fe054e881583d3d131d0d8ab333deea625b
Real1236 <arthurchan2001@live.com>
2024-08-28 17:31:34 -0400

    feature: Added monthly summary

--- output_module/exporter.py
+++ output_module/exporter.py
@@ -1,5 +1,7 @@
+import xlsxwriter.worksheet
 from filter import Filter
 import xlsxwriter
+from xlsxwriter.utility import xl_rowcol_to_cell
 from datetime import date
 from data_processing_module.statements.transaction import Transaction
 
@@ -17,8 +19,16 @@ class Exporter(Filter):
     def process(self, data: list[Transaction]) -> None:
         workbook = xlsxwriter.Workbook('SpendTracker.xlsx')
         workbook.set_size(1500, 1000)
-        worksheet = workbook.add_worksheet()
+        transactionsWorksheet = workbook.add_worksheet(name="Transactions")
+        monthlySummaryWorksheet = workbook.add_worksheet(name="Summary")
 
+        self.createHeaders(workbook, transactionsWorksheet)
+        self.recordTransactions(data, workbook, transactionsWorksheet)
+        self.createSummarySheet(workbook, monthlySummaryWorksheet)
+        
+        workbook.close()
+
+    def createHeaders(self, workbook, worksheet):
         blank_transaction = Transaction()
         header = list(vars(blank_transaction).keys())
         for i, field in enumerate(header):
@@ -26,6 +36,7 @@ class Exporter(Filter):
             worksheet.set_column_pixels(i, i, 100 if "description" not in field else 200)
         worksheet.set_row(0, cell_format=workbook.add_format({'bold': True}))
 
+    def recordTransactions(self, data: list[Transaction], workbook, worksheet):
         row = 1
         for transaction in data:
             col = 0
@@ -38,5 +49,45 @@ class Exporter(Filter):
                     worksheet.write_number(row, col, value)
                 col += 1
             row += 1
-        
-        workbook.close()
+
+    def createSummarySheet(self, workbook, worksheet):
+        # Headers
+        headers = ["Month", "Recreation", "Grocery", "Eating Out", "Transportation", "Shopping", "Miscellaneous", "One Time", "Total"]
+        for i, header in enumerate(headers):
+            worksheet.write_string(0, i, header)
+            worksheet.set_column_pixels(i, i, 100)
+        worksheet.set_row(0, cell_format=workbook.add_format({'bold': True}))
+
+        # Summary
+        for month in range(1, 13):
+            monthCell = xl_rowcol_to_cell(month, 0)
+            nextMonthCell = xl_rowcol_to_cell(month + 1, 0)
+            worksheet.write_number(month, 0, month)
+            for i in range(1, 8):
+                headerCell = xl_rowcol_to_cell(0, i)
+                worksheet.write_formula(month, i,
+                    f"""=-SUMIFS(
+                    Transactions!$F:$F,
+                    Transactions!$C:$C, ">="&DATE(2024,{monthCell},1),
+                    Transactions!$C:$C, "<"&DATE(2024,{nextMonthCell},1),
+                    Transactions!$G:$G, {headerCell}
+                    )
+                    +SUMIFS(
+                    Transactions!$F:$F,
+                    Transactions!$C:$C, ">="&DATE(2024,{monthCell},1),
+                    Transactions!$C:$C, "<"&DATE(2024,{nextMonthCell},1),
+                    Transactions!$H:$H, "Naomi",
+                    Transactions!$G:$G, {headerCell}
+                    )/2
+                    +SUMIFS(
+                    Transactions!$F:$F,
+                    Transactions!$C:$C, ">="&DATE(2024,{monthCell},1),
+                    Transactions!$C:$C, "<"&DATE(2024,{nextMonthCell},1),
+                    Transactions!$H:$H, "Mom",
+                    Transactions!$G:$G, {headerCell})"""
+                )
+            worksheet.write_formula(month, 8, f"=SUM({xl_rowcol_to_cell(month, 1)}:{xl_rowcol_to_cell(month, 7)})")
+
+            # Include 13 just for formula purposes, hidden in white
+            worksheet.write_number(13, 0, 13, workbook.add_format({'font_color': '#FFFFFF'}))
+
